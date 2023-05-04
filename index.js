8fa8f691097d0acdb00ae01f5b5d924fb234ae23
HamzaFarhat <48735251+HamzaFarhat@users.noreply.github.com>
2023-05-04 01:02:23 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,84 @@
+// var text = document.getElementById("hamza");
+// var posX = 0, posY = 0;
+// var screenWidth = document.body.clientWidth;
+// var screenHeight = document.body.clientHeight;
+
+// function moveText() {
+//     posX += (screenWidth/200);
+//     posY += (screenHeight/200);
+//     text.style.left = posX + 'px';
+//     text.style.top = posY + 'px';
+//     if (posX > screenWidth || posY > screenHeight) {
+//         clearInterval(intervalId);
+//     }
+// }
+// var intervalId = setInterval(moveText, 100);
+// setTimeout(function(){clearInterval(intervalId);},20000);
+
+
+let speed = 30;
+let scale = 0.17; // Image scale (I work on 1080p monitor)
+let canvas;
+let ctx;
+let logoColor;
+
+let dvd = {
+    x: 200,
+    y: 300,
+    xspeed: 10,
+    yspeed: 10,
+    img: new Image()
+};
+
+(function main(){
+    canvas = document.getElementById("hamza");
+    ctx = canvas.getContext("2d");
+    dvd.img.src = 'logo.png';
+
+    //Draw the "tv screen"
+    canvas.width  = window.innerWidth;
+    canvas.height = window.innerHeight;
+
+    pickColor();
+    update();
+})();
+
+function update() {
+    setTimeout(() => {
+        //Draw the canvas background
+        ctx.fillStyle = '#000';
+        ctx.fillRect(0, 0, canvas.width, canvas.height);
+        //Draw DVD Logo and his background
+        ctx.fillStyle = logoColor;
+        ctx.fillRect(dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
+        ctx.drawImage(dvd.img, dvd.x, dvd.y, dvd.img.width*scale, dvd.img.height*scale);
+        //Move the logo
+        dvd.x+=dvd.xspeed;
+        dvd.y+=dvd.yspeed;
+        //Check for collision 
+        checkHitBox();
+        update();   
+    }, speed)
+}
+
+//Check for border collision
+function checkHitBox(){
+    if(dvd.x+dvd.img.width*scale >= canvas.width || dvd.x <= 0){
+        dvd.xspeed *= -1;
+        pickColor();
+    }
+        
+    if(dvd.y+dvd.img.height*scale >= canvas.height || dvd.y <= 0){
+        dvd.yspeed *= -1;
+        pickColor();
+    }    
+}
+
+//Pick a random color in RGB format
+function pickColor(){
+    r = Math.random() * (254 - 0) + 0;
+    g = Math.random() * (254 - 0) + 0;
+    b = Math.random() * (254 - 0) + 0;
+
+    logoColor = 'rgb('+r+','+g+', '+b+')';
+}
